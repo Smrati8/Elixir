0e7cbb34495aefafd30181cb329d283286ed8a07
Smrati8 <56731723+Smrati8@users.noreply.github.com>
2019-10-19 21:42:17 -0400

    Calling through Supervisor Sorted

--- project1/proj1.exs
+++ project1/proj1.exs
@@ -1,21 +1,25 @@
 defmodule Proj1 do
   def vampnum do
+    #Input the number from the console
     number = System.argv()
     [num1 | num2] = number
     number1 = String.to_integer(num1)
     number2 = String.to_integer(Enum.join(num2))
+    #Invoking the Supervisor
     {:ok, supervisor_id} = Vampiresupervisor.start_link(number1, number2)
 
     childrens = Supervisor.which_children(supervisor_id)
 
-    Enum.each(childrens, fn {_id, pid, _type, _module} ->
+    #Call made to the childrens to get the details from Pid and print the data
+    Enum.each(Enum.sort(childrens), fn {_id, pid, _type, _module} ->
       if GenServer.call(pid, :get_state) != [] do
-        IO.inspect(GenServer.call(pid, :get_state))
+        IO.puts(GenServer.call(pid, :get_state))
       end
     end)
   end
 end
 
+#Module created for Supervisor
 defmodule Vampiresupervisor do
   use Supervisor
 
@@ -38,6 +42,7 @@ end
 defmodule Vampirenumber do
   use GenServer
 
+  #Initiating the Genserver
   def start_link(list) do
     {:ok, pid} = GenServer.start_link(__MODULE__, nil)
     GenServer.cast(pid, {:calculate, list})
@@ -52,6 +57,7 @@ defmodule Vampirenumber do
     {:reply, state, state}
   end
 
+  #passing the factors method that calculate the fangs of the number
   def handle_cast({:calculate, list}, _state) do
     values =
       Enum.map(list, fn n ->
@@ -66,16 +72,22 @@ defmodule Vampirenumber do
       end)
 
     result = Enum.filter(values, fn r -> r != "" end)
-    {:noreply, result}
+    if length(result) > 1 do
+      {:noreply, Enum.join(result, "\n")}
+    else
+      {:noreply, result}
+    end
   end
 
+  #Check if the digit count is even
   def digcount(number) do
     rem(length(Integer.digits(number)), 2) == 0
   end
 
+  #calculating the fangs of the number
   def factors(number) do
     split_length = div(length(to_charlist(number)), 2)
-    first = round(number / :math.pow(10, split_length - 1))
+    first = round(number / :math.pow(10, split_length))
     last = round(:math.sqrt(number))
 
     values =
@@ -109,10 +121,12 @@ defmodule Vampirenumber do
     values
   end
 
+  #checking if the permuattion of the fangs matches the number
   def compare(number) do
     Enum.sort(Enum.chunk_every(Integer.to_charlist(number), 1))
   end
 
+  #Checking if both the fangs are not having trailing zeros
   def trailzero(factor1, factor2) do
     rem(factor1, 10) == 0 && rem(factor2, 10) == 0
   end
